93d5c78444c82c210848d2f03fd349341c2d0572
TunrayoAk <motunrakande@gmail.com>
2020-07-05 21:14:38 +0100

    My ASPMVC BMI

---
+++ BMICC/Controllers/CalccController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using BMICC.Models;
+
+namespace BMICC.Controllers
+{
+    public class CalccController : Controller
+    {
+        // GET: Calcc
+        public ActionResult Index()
+        {
+            return View(new calc());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(calc c, string calculate)
+        {
+            Math.Round(c.result, 2);
+            c.result = c.weight / (c.height / 100 * c.height / 100);
+
+            if(c.result < 18.5)
+            {
+                c.implication = "You are underweight";
+            }
+            else if(c.result <= 24.9)
+            {
+                c.implication = "You are healthy";
+            }
+            else if(c.result <= 29.9)
+            {
+                c.implication = "You are overweight";
+
+            }
+            else
+            {
+                c.implication = "You are obese";
+            }
+
+            return View(c);
+        }
+    }
+}
---
+++ BMICC/Models/calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BMICC.Models
+{
+    public class calc
+    {
+        public double weight { get; set; }
+
+        public double height { get; set; }
+
+        public double result { get; set; }
+
+        public string implication { get; set; }
+    }
+}
